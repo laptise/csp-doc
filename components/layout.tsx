0cb30a7b317673ea2aab321c56c3ad381ad17799
Yoonsoo Kim <laptise@live.jp>
2022-05-21 19:36:51 +0900

    initial

---
+++ components/layout.tsx
@@ -0,0 +1,31 @@
+import { FC, ReactNode } from "react";
+import styles from "../styles/Home.module.css";
+import { Header } from "./header";
+import SearchAppBar from "./nav";
+
+type Props = {
+  title: string;
+  children: ReactNode;
+};
+
+export const Layout: FC<Props> = ({ title, children }) => {
+  return (
+    <>
+      <SearchAppBar />
+      <div className={styles.container}>
+        <Header title={title} />
+        {children}
+      </div>
+      <main>{children}</main>
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by <span className={styles.logo}></span>
+        </a>
+      </footer>
+    </>
+  );
+};
